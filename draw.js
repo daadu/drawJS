cd45448d69039b5f2e2b8474e458be9187a65e06
EXCELSIOR <harsh.bhikadiya@gmail.com>
2014-03-09 01:29:13 +0530

    removed hammerJS, add HTML5 Touch API, redrawing only after event triggered, feddback when rect is being drawn and moved

--- draw.js
+++ draw.js
@@ -1,6 +1,15 @@
-
-
 window.addEventListener("load",eventWindowLoaded,false);
+var deviceAgent = navigator.userAgent.toLowerCase();
+var isTouchDevice = ('ontouchstart' in document.documentElement) || 
+					(deviceAgent.match(/(iphone|ipod|ipad)/) ||
+					deviceAgent.match(/(android)/)  || 
+					deviceAgent.match(/(iemobile)/) || 
+					deviceAgent.match(/iphone/i) || 
+					deviceAgent.match(/ipad/i) || 
+					deviceAgent.match(/ipod/i) || 
+					deviceAgent.match(/blackberry/i) || 
+					deviceAgent.match(/bada/i));
+
 function eventWindowLoaded(){
 	canvasApp();
 }
@@ -11,90 +20,110 @@ function canvasApp(){
 	theCanvas.height = 200;
 	var tem1,tem2;
 	var drawings = [];
-	var selectedDrawing;
-	//theCanvas.addEventListener("mousedown",mouseDown,false);
-	//theCanvas.addEventListener("mouseup",mouseUp,false);
-	hammertimetouch = Hammer(theCanvas).on("touch",canvasTouched);
-	hammertimerelease = Hammer(theCanvas).on("release",canvasReleased);
-	//theCanvas.addEventListener("mouseover",mouseOver,false);
+	var selectedDrawing,newDrawing;
+	theCanvas.addEventListener("mousedown",canvasMouseDown,false);
+	//theCanvas.addEventListener("mousemove",doNothing,false);
+	if(isTouchDevice){
+		theCanvas.addEventListener("touchstart",canvasTouchStart,false);
+	}
 	var cat = new Image();
 	cat.src = "cat.jpeg";
-	function canvasTouched(e){
-		e.gesture.preventDefault()
-		if(!selectedDrawing){
-			selectDrawing(e.gesture.center.pageX,e.gesture.center.pageY)
-		}
-		tem1 = e.gesture.center.pageX;
-		tem2 = e.gesture.center.pageY;
-	}
-	function mouseDown(e){
-		//console.log(e.clientX,e.clientY);
-		if(!selectedDrawing){
-			selectDrawing(e.clientX,e.clientY);
-		}
-		
+	function canvasMouseDown(e){
+		selectDrawing(e.clientX,e.clientY);
 		tem1 = e.clientX;
 		tem2 = e.clientY;
+		theCanvas.addEventListener("mousemove",canvasMouseMove,false);
+		newDrawing = new drawObject(e.clientX,e.clientY,e.clientX,e.clientY);
+		drawings.push(newDrawing)
+		function canvasMouseMove(e){
+			//console.log(e)
+			if(selectedDrawing){
+				selectedDrawing.reposition(e.clientX,e.clientY);
+			}else{
+				if(newDrawing){
+					newDrawing.resize(tem1,tem2,e.clientX,e.clientY);
+				}
+			}
+			drawScreen();
+			theCanvas.addEventListener("mouseup",canvasMouseUp,false);
+			function canvasMouseUp(e){
+				newDrawing = null;
+				selectedDrawing = null;
+			}
+		}
+	}
+	function canvasTouchStart(e){
+		e.preventDefault();
+		selectDrawing(e.targetTouches[0].pageX,e.targetTouches[0].pageY)
+		tem1 = e.targetTouches[0].pageX;
+		tem2 = e.targetTouches[0].pageY;
+		theCanvas.addEventListener("touchmove",canvasTouchMove,false);
+		newDrawing = new drawObject(e.targetTouches[0].pageX,e.targetTouches[0].pageY,e.targetTouches[0].pageX,e.targetTouches[0].pageY);
+		drawings.push(newDrawing);
+		function canvasTouchMove(e){
+			e.preventDefault();
+			if(selectedDrawing){
+				selectedDrawing.reposition(e.targetTouches[0].pageX,e.targetTouches[0].pageY);
+			}else{
+				if(newDrawing){
+					newDrawing.resize(tem1,tem2,e.targetTouches[0].pageX,e.targetTouches[0].pageY);
+				}
+			}
+			drawScreen();
+			theCanvas.addEventListener("touchend",canvasTouchEnd,false);
+			function canvasTouchEnd(e){
+				newDrawing = null;
+				selectedDrawing = null;
+			}
+		}
 	}
 	function selectDrawing(x,y){
 		if(drawings){
 			for(var i=0;i<drawings.length;i++){
 				selectedDrawing = false;
-				var threshold = 10;
-				var left = (drawings[i].fromX<drawings[i].toX)?drawings[i].fromX:drawings[i].toY;
-				var right = (left==drawings[i].fromX)?drawings[i].toX:drawings[i].fromX;
-				var top = (drawings[i].fromY<drawings[i].toY)?drawings[i].fromY:drawings[i].toY;
-				var bottom = (top==drawings[i].fromY)?drawings[i].toY:drawings[i].fromY;
-				if(((x<left+threshold&&x>left-threshold)&&(y>top-threshold )&&(y<bottom+threshold)) ||
-					((x<right+threshold)&&(x>right-threshold)&&(y>top-threshold )&&(y<bottom+threshold)) ||
-					((y<top+threshold)&&(y>top-threshold)&&(x>left-threshold)&&(x<right+threshold)) ||
-					((y<bottom+threshold)&&(y>bottom-threshold)&&(x>left-threshold)&&(x<right+threshold))
+				var th = 10; //threshold
+				var l = drawings[i].left;
+				var r = drawings[i].right;
+				var t = drawings[i].top;
+				var b = drawings[i].bottom;
+				if(((x<l+th&&x>l-th)&&(y>t-th )&&(y<b+th)) ||
+					((x<r+th)&&(x>r-th)&&(y>t-th )&&(y<b+th)) ||
+					((y<t+th)&&(y>t-th)&&(x>l-th)&&(x<r+th)) ||
+					((y<b+th)&&(y>b-th)&&(x>l-th)&&(x<r+th))
 					){
-					selectedDrawing = drawings[i]
+					selectedDrawing =  drawings[i];
 					return 
 				}
 			}
 		}
 	}
-	function canvasReleased(e){
-		e.gesture.preventDefault()
-		if(!selectedDrawing){
-			drawings.push(new drawnObject(tem1,tem2,e.gesture.center.pageX,e.gesture.center.pageY));
-			
-		}else{
-			var width = selectedDrawing.fromX - selectedDrawing.toX;
-			width = (width<0)?-1*width:width;
-			var height = selectedDrawing.fromY - selectedDrawing.toY;
-			height = (height<0)?-1*height:height;
-			selectedDrawing.fromX = e.gesture.center.pageX;
-			selectedDrawing.fromY = e.gesture.center.pageY;
-			selectedDrawing.toX = selectedDrawing.fromX+width;
-			selectedDrawing.toY = selectedDrawing.fromY+height;
-			selectedDrawing = null;
+	function drawObject(a,b,c,d){
+		this.left = (a<c)?a:c;
+		this.right = (this.left==a)?c:a;
+		this.top = (b<d)?b:d;
+		this.bottom = (this.top==b)?d:b;
+		this.width = this.right - this.left;
+		this.height = this.bottom - this.top;
+		this.reposition = function(x,y){
+			this.left = x;
+			this.right = this.left+this.width;
+			this.top = y;
+			this.bottom = this.top+this.height;
+			/*var dx = refX-this.left;
+			var dy = refY-this.top;
+			this.left += dx;
+			this.top += dy;
+			this.right -= dx;
+			this.bottom -= dy;*/
 		}
-	}
-	function mouseUp(e){
-		if(!selectedDrawing){
-			drawings.push(new drawnObject(tem1,tem2,e.clientX,e.clientY));
-			
-		}else{
-			var width = selectedDrawing.fromX - selectedDrawing.toX;
-			width = (width<0)?-1*width:width;
-			var height = selectedDrawing.fromY - selectedDrawing.toY;
-			height = (height<0)?-1*height:height;
-			selectedDrawing.fromX = e.clientX;
-			selectedDrawing.fromY = e.clientY;
-			selectedDrawing.toX = selectedDrawing.fromX+width;
-			selectedDrawing.toY = selectedDrawing.fromY+height;
-			selectedDrawing = null;
+		this.resize = function(fromX,fromY,toX,toY){
+			this.left = (fromX<toX)?fromX:toX;
+			this.right = (this.left==fromX)?toX:fromX;
+			this.top = (fromY<toY)?fromY:toY;
+			this.bottom = (this.top==fromY)?toY:fromY;
+			this.width = this.right - this.top;
+			this.height = this.bottom - this.top;
 		}
-		//delete tempDrawing;
-	}
-	function drawnObject(a,b,c,d){
-		this.fromX = a;
-		this.fromY = b;
-		this.toX = c;
-		this.toY = d;
 	}
 	function drawScreen(){
 		theCanvas.width = theCanvas.width;
@@ -107,36 +136,33 @@ function canvasApp(){
 	function render() {
 		for(var i=0;i<drawings.length;i++){
 			if(selectedDrawing == drawings[i]){
-				context.save();
-				context.lineWidth =1;
-				context.beginPath();
-				context.moveTo(drawings[i].fromX,drawings[i].fromY);
-				context.lineTo(drawings[i].toX,drawings[i].fromY);
-				context.lineTo(drawings[i].toX, drawings[i].toY);
-				context.lineTo(drawings[i].fromX,drawings[i].toY);
-				context.lineTo(drawings[i].fromX,drawings[i].fromY);
-				context.strokeStyle = "#FF0000";
-				context.stroke();
-				context.restore();
+				drawShape("rectangle",drawings[i])
 			}else{
-				context.save();
-				context.lineWidth =1;
-				context.beginPath();
-				context.moveTo(drawings[i].fromX,drawings[i].fromY);
-				context.lineTo(drawings[i].toX,drawings[i].fromY);
-				context.lineTo(drawings[i].toX, drawings[i].toY);
-				context.lineTo(drawings[i].fromX,drawings[i].toY);
-				context.lineTo(drawings[i].fromX,drawings[i].fromY);
-				context.stroke();
-				context.restore();
+				drawShape("rectangle",drawings[i])	
 			}
 		}
 	}
-	function calcDistance(x1,y1,x2,y2){
-		return Math.floor(Math.sqrt( (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) ))
+	function drawShape(type,drawObj){
+		switch(type){
+			case "rectangle" : 
+				context.save();
+				context.lineWidth =1;
+				context.beginPath();
+				context.moveTo(drawObj.left,drawObj.top);
+				context.lineTo(drawObj.right,drawObj.top);
+				context.lineTo(drawObj.right, drawObj.bottom);
+				context.lineTo(drawObj.left,drawObj.bottom);
+				context.lineTo(drawObj.left,drawObj.top);
+				if(selectedDrawing == drawObj){
+					context.strokeStyle = "#FF0000";
+				}
+				context.stroke();
+				context.restore();
+				break;
+		}
 	}
 	function Loop() {
-		window.setTimeout(Loop, 20);
+		//window.setTimeout(Loop, 20);
 		drawScreen();
 	}
 	Loop();
